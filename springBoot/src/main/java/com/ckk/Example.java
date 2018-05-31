61b39de8e5eef26b8c123b0173a18c44649b4af7
Collyer <Collyer@CDS-5>
2018-05-31 10:22:52 +0800

    init

---
+++ springBoot/src/main/java/com/ckk/Example.java
@@ -0,0 +1,27 @@
+package com.ckk;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ckk.model.Cat;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+	@RequestMapping("/hi")
+    String home() {
+        return "Hello World! ";
+    }
+	
+	@RequestMapping("/json")
+	Cat json() {
+		
+		Cat cat = new Cat();
+		cat.setName("miao");
+		cat.setGender("male");
+		
+        return cat; 
+    }
+}
---
+++ springBoot/src/main/java/com/ckk/model/Cat.java
@@ -0,0 +1,39 @@
+package com.ckk.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cat {
+
+	private String name;
+	private String gender;
+	private List<String> kids = new ArrayList<>();
+	
+	public Cat() {
+		kids.add("miya");
+		kids.add("萌猫");
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public List<String> getKids() {
+		return kids;
+	}
+	public void setKids(List<String> kids) {
+		this.kids = kids;
+	}
+	
+	
+	
+}
